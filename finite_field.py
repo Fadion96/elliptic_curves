024ac40ca12b4a16809ccc5ff368d46d8bce9505
lukaszklekowski <lukasz.k.klekowski@gmail.com>
2020-03-05 02:15:16 +0100

    Add Fp

---
+++ finite_field.py
@@ -0,0 +1,45 @@
+# def egcd(a, b):
+#     lastremainder, remainder = abs(a), abs(b)
+#     x, lastx, y, lasty = 0, 1, 1, 0
+#     while remainder:
+#         lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+#         x, lastx = lastx - quotient*x, x
+#         y, lasty = lasty - quotient*y, y
+#     return lastremainder, lastx * (-1 if a < 0 else 1), lasty * (-1 if b < 0 else 1)
+
+# def modinv(b, n):
+# 	g, x, _ = egcd(b, n)
+# 	if g == 1:
+# 		return x % n
+
+
+
+def F(p):
+    class F:
+        def __init__(self, x):
+            self.int = x % p
+
+        def __str__(self):
+            return str(self.int)
+
+        __repr__ = __str__
+
+        def __eq__(self, b):
+            return self.int == b.int
+
+        def __ne__(self, b):
+            return self.int != b.int
+
+        def __add__(self, b):
+            return F(self.int + b.int)
+
+        def __sub__(self, b):
+            return F(self.int - b.int)
+
+        def __mul__(self, b):
+            return F(self.int * b.int)
+
+        def __truediv__(self, b):
+            return self*F(pow(b.int, p-2, p))
+
+    return F
--- pollard_rho.py
+++ pollard_rho.py
@@ -1,41 +1,39 @@
-def egcd(a, b):
-    lastremainder, remainder = abs(a), abs(b)
-    x, lastx, y, lasty = 0, 1, 1, 0
-    while remainder:
-        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
-        x, lastx = lastx - quotient*x, x
-        y, lasty = lasty - quotient*y, y
-    return lastremainder, lastx * (-1 if a < 0 else 1), lasty * (-1 if b < 0 else 1)
+from finite_field import F
+import gensafeprime
+import random
 
-def modinv(b, n):
-	g, x, _ = egcd(b, n)
-	if g == 1:
-		return x % n
+def step(A, alpha, beta, g, y, F_ord_p):
+    if A.int % 3 == 0:
+        return (A * A), (F_ord_p(2) * alpha), (F_ord_p(2) * beta)
+    elif A.int % 3 == 1:
+        return (A * y), alpha, (beta + F_ord_p(1))
+    elif A.int % 3 == 2:
+        return (g * A), (alpha + F_ord_p(1)), beta
 
-def step(A, alpha, beta, g, y, p, ord_p):
-    if A % 3 == 0:
-        return (A * A) % p, (2 * alpha) % ord_p, (2 * beta) % ord_p
-    elif A % 3 == 1:
-        return (A * y) % p, alpha, (beta + 1) % ord_p
-    elif A % 3 == 2:
-        return (g * A) % p, (alpha + 1) % ord_p, beta
+def pollard_rho(g, y, F_p, F_ord_p):
 
-def pollard_rho(g, y, p, ord_p):
-    A = 1
-    B = 1
-    alpha_A, beta_A = 0, 0
-    alpha_B, beta_B = 0, 0
+    A = F_p(1)
+    B = F_p(1)
+    alpha_A, beta_A = F_ord_p(0), F_ord_p(0)
+    alpha_B, beta_B = F_ord_p(0), F_ord_p(0)
     while True:
-        A, alpha_A, beta_A = step(A, alpha_A, beta_A, g, y, p, ord_p)
-        B, alpha_B, beta_B = step(B, alpha_B, beta_B,g, y, p, ord_p)
-        B, alpha_B, beta_B = step(B, alpha_B, beta_B,g, y, p, ord_p)
+        A, alpha_A, beta_A = step(A, alpha_A, beta_A, g, y, F_ord_p)
+        B, alpha_B, beta_B = step(B, alpha_B, beta_B, g, y, F_ord_p)
+        B, alpha_B, beta_B = step(B, alpha_B, beta_B,g, y, F_ord_p)
         if A == B:
-            x = (alpha_B - alpha_A) * modinv(beta_A - beta_B, ord_p) % ord_p
-            return x 
+            x = (alpha_B - alpha_A) / (beta_A - beta_B)
+            yield x 
         
-    
-x = pollard_rho(2, 5, 1019, 509)
-assert pow(2,x, 1019) == 5
 
+for i in range(50):
+    print(i)
+    safeprime = gensafeprime.generate(30)
+    F_p = F(safeprime)
+    F_ord_p = F((safeprime-1)//2)
+    y = random.randint(2, safeprime - 1)
 
-    
+    xx = pollard_rho(F_p(2), F_p(y), F_p, F_ord_p)
+
+    for x in xx:
+        if pow(2, x.int, safeprime) == y:
+            break
